1f6c55b4aa3833e6683be170b2afe356a0a98983
Aditya <adityamittal307@gmail.com>
2023-05-27 18:23:00 -0700

    add sidebar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
 import './App.css';
+import Business from './components/business';
 import BusinessList from './components/businessList';
 import SearchBar from './components/searchBar';
 import Header from './components/header';
 import React, { useState, useEffect } from 'react';
+import './sidebar.css'; // Sidebar functionality
 
 function App() {
   const [businesses, setBusinesses] = useState([]);
   const [saved, setSaved] = useState([]);
+  const [leftOpen, setLeftOpen] = useState(false);
 
+  
   useEffect(() => {
     console.log(saved);
   }, [saved]);
 
+  function toggleSidebar(event) {
+    setLeftOpen(prev => !prev);
+  }
+
+  let leftO = leftOpen ? 'open' : 'closed';
+
   return (
     <div className="App">
-      <Header />
-      <SearchBar setBusinesses={setBusinesses}/>
-      <BusinessList businesses={businesses} setSaved={setSaved}/>
+      <div id="layout">
+        <div id='left' className={leftO} >
+          <button className='icon'
+              onClick={toggleSidebar} >Saved
+          </button>
+          <div className={`sidebar ${leftO}`} >
+            <div className='header'>
+            </div>
+            <div className='content sidebar-content'>
+                {saved.map(data => <Business businessInfo={data} setSaved={setSaved}/>)}
+            </div>
+          </div>
+        </div>
+
+        <div id="main">
+          <div className='header'>
+              <div className="header-bg">
+                  <h1 className={`header-title title${'left-'+leftO}`}>ravenous</h1>
+              </div>
+          </div>
+          <div className='content'>
+              <SearchBar setBusinesses={setBusinesses}/>
+              <BusinessList businesses={businesses} setSaved={setSaved} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
