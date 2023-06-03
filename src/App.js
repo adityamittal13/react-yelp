587411f6e442cddd0bd8bd0e694ed3181978679b
Aditya <adityamittal307@gmail.com>
2023-06-03 15:11:18 -0700

    revert to old code

--- src/App.js
+++ src/App.js
@@ -1,42 +1,33 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import SavedBusiness from './components/savedBusiness';
 import BusinessList from './components/businessList';
 import SearchBar from './components/searchBar';
-<<<<<<< HEAD
-import React, { useState, useEffect } from 'react';
-=======
 import LoginForm from './components/LoginForm';
 // import retrieveUserAccounts from './utils/server';
 import users from './data/users';
 import './App.css';
->>>>>>> routing
 import './sidebar.css'; // Sidebar functionality
 
 function App() {
   const [businesses, setBusinesses] = useState([]);
   const [saved, setSaved] = useState([]);
   const [leftOpen, setLeftOpen] = useState('closed');
+  const [login, setLogin] = useState({
+    inLogin: true,
+    email: '',
+    password: ''
+  });
+  const [accounts, setAccounts] = useState(users);
 
-  
   useEffect(() => {
-    console.log(saved);
-  }, [saved]);
+    console.log(users);
+  }, [accounts])
 
   function toggleSidebar(event) {
     setLeftOpen(prev => (prev === 'open') ? 'closed' : 'open');
   }
 
 
-<<<<<<< HEAD
-  return (
-    <div className="App">
-      <div id="layout">
-        <div id='left' className={leftOpen} >
-          <button className='icon'
-              onClick={toggleSidebar} >Saved
-          </button>
-          <div className={`sidebar ${leftOpen}`} >
-=======
   function submitHandler(event) {
     event.preventDefault();
 
@@ -70,35 +61,57 @@ function App() {
       <div className="App">
         <div id="layout">
           <div id="main">
->>>>>>> routing
             <div className='header'>
-            </div>
-            <div className='content sidebar-content line'>
-                <h2 className="header">{`Saved Count: ${saved.length}`}</h2>
-                {saved.map(data => (
-                  <div>
-                    <SavedBusiness businessInfo={data}/>
-                    <br/>
-                  </div>
-                ))}
-            </div>
-          </div>
-        </div>
-
-        <div id="main">
-          <div className='header'>
-              <div className="header-bg">
+                <div className="header-bg">
                   <h1 className={`header-title title${'left-'+leftOpen}`}>ravenous</h1>
-              </div>
-          </div>
-          <div className='content'>
-              <SearchBar setBusinesses={setBusinesses}/>
-              <BusinessList businesses={businesses} setSaved={setSaved} />
+                </div>
+            </div>
+            <div className="content">
+              <LoginForm setLogin={setLogin} accounts={accounts} setAccounts={setAccounts} setSaved={setSaved}/>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="App">
+        <div id="layout">
+          <div id='left' className={leftOpen} >
+            <button className='icon left-margin'
+                onClick={toggleSidebar} >Saved
+            </button>
+            <button className='icon' onClick={submitHandler}>Sign Out</button>
+            <div className={`sidebar ${leftOpen}`} >
+              <div className='header'>
+              </div>
+              <div className='content sidebar-content line'>
+                  <h2 className="header">{`Saved Count: ${saved.length}`}</h2>
+                  {saved.map(data => (
+                    <div>
+                      <SavedBusiness businessInfo={data}/>
+                      <br/>
+                    </div>
+                  ))}
+              </div>
+            </div>
+          </div>
+  
+          <div id="main">
+            <div className='header'>
+                <div className="header-bg">
+                  <h1 className={`header-title title${'left-'+leftOpen}`}>ravenous</h1>
+                </div>
+            </div>
+            <div className='content'>
+                <SearchBar setBusinesses={setBusinesses}/>
+                <BusinessList businesses={businesses} saved={saved} setSaved={setSaved} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
